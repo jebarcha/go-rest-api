7dc82e3410cd2551c036a50756b5731d67a1ae95
jebarcha <jebarcha@hotmail.com>
2024-07-15 11:46:09 -0600

    adding middleware for token validation

--- routes/routes.go
+++ routes/routes.go
@@ -1,13 +1,24 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"rest-api/middlewares"
+
+	"github.com/gin-gonic/gin"
+)
 
 func RegisterRoutes(server *gin.Engine) {
 	server.GET("/events", getEvents)    //GET, POST, PUT, PATCH, DELETE
 	server.GET("/events/:id", getEvent) // /events/1, /events/5
-	server.POST("/events", createEvent)
-	server.PUT("/events/:id", updateEvent)
-	server.DELETE("/events/:id", deleteEvent)
+
+	authenticated := server.Group("/") //create a group of routes
+	authenticated.Use(middlewares.Authenticate)
+	authenticated.POST("/events", createEvent)
+	authenticated.PUT("/events/:id", updateEvent)
+	authenticated.DELETE("/events/:id", deleteEvent)
+	// server.POST("/events", middlewares.Authenticate, createEvent)
+	// server.PUT("/events/:id", updateEvent)
+	// server.DELETE("/events/:id", deleteEvent)
+
 	server.POST("/signup", signup)
 	server.POST("/login", login)
 }
